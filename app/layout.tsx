531aea4b61ae0231917ca3bb0fdcfee8095f136d
naufal aufa <muhammadnaufalaufarifqi@gmail.com>
2024-11-06 16:14:04 +0700

    delete: delete import footername error second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
-import FooterName from "@/components/FooterName";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
